c766816e2191032736f615b090067934c2546664
jesse tweedle <jesse.tweedle@gmail.com>
2025-10-27 09:37:08 -0300

    deploy 2025-10-27T12:37:08Z

--- viewer/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/ttc-deckgl/", // 👈 must match your GitHub repo name
-});
